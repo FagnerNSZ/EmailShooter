da1901b309d47445bff14d586bfacb971a329353
fagner <fagnerns.vix@hotmail.com>
2018-06-15 06:57:49 -0300

    first commit

---
+++ EmailShooter/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// This projetct 
+/// </summary>
+namespace EmailShooter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                String CredentialEmailUser = "eu@email.com";
+                String CredentialPasswordEmail = "123456";
+                String mail1 = "suporte@outlook.com";
+                String mail2 = "fnascimento @outlook.com.br";
+
+              bool resposta = ShooterOutlook.ShooterEmail(CredentialEmailUser, CredentialPasswordEmail, mail1, mail2);
+                if (resposta != false)
+                {
+                    Console.WriteLine("Email enviado com sucesso!");
+                }
+                else {
+                    Console.WriteLine("Erro no envio!");
+                }
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+        
+
+        }
+    }
+}
---
+++ EmailShooter/ShooterOutlook.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmailShooter
+{
+    public class ShooterOutlook
+    {
+
+        public static bool ShooterEmail(String CredentialEmailUser, String CredentialPasswordEmail, String mail1, string mail2)
+        {
+            SmtpClient client = new SmtpClient("smtp.live.com");  // use smtp-mail.outlook.com in the case use smtp
+
+            client.Port = 587;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            System.Net.NetworkCredential credentials =
+                new System.Net.NetworkCredential("ricardopulini@outlook.com", "minhasenha");
+            client.EnableSsl = true;
+            client.Credentials = credentials;
+
+            try
+            {
+                //Aqui deve ser criado um metodo para tratar e copular uma lista de emails
+                //var mail = new MailMessage("suporte@outlook.com", "fnascimento@outlook.com.br");
+                var mail = new MailMessage("suporte@outlook.com", "fnascimento@outlook.com.br");
+
+                mail.Subject = "Aviso de problema";
+                mail.Body = "Ocorreu um problema no servidor verifique!";
+                client.Send(mail);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                throw ex;
+            }
+
+            return false;
+        }
+
+
+    }
+   }
